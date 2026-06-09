82344ae893f823bcaefb139d9fc77c34d1b9047c
Doruk Bilge <dorukbilge05@gmail.com>
2026-06-09 09:06:41 -0400

    implemented synthetic dataset and DataLoader

--- src/torchmlp/__init__.py
+++ src/torchmlp/__init__.py
@@ -1,3 +1,16 @@
+from torchmlp.data import (
+    SurfaceDataset,
+    create_surface_dataloaders,
+    create_surface_datasets,
+    sample_surface_points,
+)
 from torchmlp.model import ActivationName, MLP
 
-__all__ = ["ActivationName", "MLP"]
+__all__ = [
+    "ActivationName",
+    "MLP",
+    "SurfaceDataset",
+    "create_surface_dataloaders",
+    "create_surface_datasets",
+    "sample_surface_points",
+]
